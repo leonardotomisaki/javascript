1fb358b5c3609180f580becc9e977e166b29f7f1
Leonardo Kazuhiro Tomisaki <168482917+leonardotomisaki@users.noreply.github.com>
2024-05-12 18:52:22 -0300

    exercicios

--- aula12ex/ex014/script.js
+++ aula12ex/ex014/script.js
@@ -1,1 +1,29 @@
-alert('Olá')
+function carregar() {
+    var msg = window.document.getElementById('msg')
+    var img = window.document.getElementById('imagem')
+    var data = new Date()
+    var boa = document.getElementById('boa')
+    //var hora = data.getHours()
+    var hora = 10
+    msg.innerHTML = `Agora são ${hora} horas!`
+    if (hora >= 0 && hora < 12){
+        // BOM DIA!
+        img.src = 'imagens/fotomanha.png'
+        boa.innerHTML = ('Bom Dia!') 
+        document.body.style.background = '#fead4b'
+    } else if (hora >= 12 && hora < 18){
+        // BOA TARDE!
+        img.src = 'imagens/fototarde.png'
+        boa.innerHTML = ('Boa Tarde!') 
+        document.body.style.background = '#f2dec5'
+    } else {
+        // BOA NOITE!
+        img.src = 'imagens/fotonoite.png'
+        boa.innerHTML = ('Boa Noite!')
+        document.body.style.background = '#24252a'
+    }
+        
+
+        
+
+}
---
+++ aula12ex/ex015/script.js
@@ -0,0 +1,48 @@
+function verificar() {
+    var data = new Date()
+    var ano = data.getFullYear()
+    var fano = document.getElementById('txtano')
+    var res = document.querySelector('div#res')
+    if (fano.value.length == 0 || fano.value > ano ) {
+        alert('[ERRO] Verifique os dados e tente novamente!')
+    } else {
+    var fsex = document.getElementsByName('radsex')
+    var idade = ano - Number(fano.value)
+    var genero = ''
+    var imagem = document.createElement('img')
+    imagem.setAttribute('id','foto')
+    if (fsex[0].checked) {
+        genero = 'Homem'
+        if(idade >= 0 && idade < 12) {
+            //Criança
+            imagem.setAttribute('src','imagens/criança-m.png')
+        } else if (idade >= 12 && idade < 22) {
+            //Jovem
+            imagem.setAttribute('src','imagens/jovem-m.png')
+        } else if (idade > 22 && idade < 60){
+            //Adulto
+            imagem.setAttribute('src','imagens/adulto-m.png')
+        } else {
+            //Idoso
+            imagem.setAttribute('src','imagens/idoso-m.png')
+        }
+    } else if (fsex[1].checked) {
+        genero = 'Mulher'
+        if(idade >= 0 && idade < 12) {
+            //Criança
+            imagem.setAttribute('src','imagens/criança-f.png')
+        } else if (idade >= 12 && idade < 22) {
+            //Jovem
+            imagem.setAttribute('src','imagens/jovem-f.png')
+        } else if (idade > 22 && idade < 60){
+            //Adulto
+            imagem.setAttribute('src','imagens/adulto-f.png')
+        } else {
+            //Idoso
+            imagem.setAttribute('src','imagens/idoso-f.png')
+        }
+    }
+        res.innerHTML = (`Detectamos ${genero} com ${idade} anos!`)
+        res.appendChild(imagem)
+    }
+}
