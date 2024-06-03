61f61b88f8677c76430e29b53852180c4d7fe396
Leonardo Kazuhiro Tomisaki <168482917+leonardotomisaki@users.noreply.github.com>
2024-06-03 19:56:20 -0300

    atualização

---
+++ aula13/ambiente.js
@@ -0,0 +1,16 @@
+var c = 1
+while(c <= 5)
+    console.log('Tudo Bem?')
+    c++
+
+
+/*
+console.log('Tudo Bem?')
+console.log('Tudo Bem?')
+console.log('Tudo Bem?')
+console.log('Tudo Bem?')
+console.log('Tudo Bem?')
+console.log('Tudo Bem?')
+console.log('Tudo Bem?')
+console.log('Tudo Bem?')
+*/
